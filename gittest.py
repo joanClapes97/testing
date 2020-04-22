771411367910b85107af18db1908689ca32b99ee
joanClapes97 <joanClapes97@users.noreply.github.com>
2020-04-22 14:16:58 +0200

    adding starting project

---
+++ gittest.py
@@ -0,0 +1,11 @@
+"""
+We work locally here and we have a repository on our githb account.
+Now, clone the repository on the same directory as here, /Joan.
+We work on this file/project, but also New_employee works on that from another
+folder which can be in any computer. Let's do it (we are Joan)
+Use Git Bash on Windows
+Once cloned, put our files inside the repository we cloned.
+
+# NOTE: the repository contains a hided directory, .git, which allows
+us to communicate with it
+"""
